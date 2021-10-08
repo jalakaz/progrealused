29f2af0c215afc6459b74566984065f462162910
jalakaz <92162133+jalakaz@users.noreply.github.com>
2021-10-08 15:44:07 +0300

    Create checkTemp.js

---
+++ homework/checkTemp.js
@@ -0,0 +1,14 @@
+function checkTemp(temp){
+    if(temp<=20)
+    {
+        return -1
+    }
+    else if(temp>=21 && temp<=40)
+    {
+        return 0
+    }
+    else if(temp>=41 && temp<=60)
+    {
+        return 1
+    }
+}
